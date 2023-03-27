bf41a471ec3c0d2ce9c0f578543d7ffde0b31ac6
jamico <jamicojerahmeel@gmailcom>
2023-03-27 20:21:37 +0100

    fix bug

--- controllers/appController.js
+++ controllers/appController.js
@@ -113,7 +113,7 @@ export async function login(req, res) {
                 userId: user._id,
                 username: user.username,
               },
-              process.env.JWT_SECRET,
+              "secret",
               { expiresIn: "24h" }
             );
 
@@ -138,21 +138,23 @@ export async function login(req, res) {
 /** GET: http://localhost:8080/api/user/example*/
 export async function getUser(req, res) {
   const { username } = req.params;
-  try {
-    if (!username) return res.status(501).snd({ error: "Invalid Username" });
 
-    UserModel.findOne({ username }, (err, user) => {
+  try {
+    if (!username) return res.status(501).send({ error: "Invalid Username" });
+
+    UserModel.findOne({ username }, function (err, user) {
       if (err) return res.status(500).send({ err });
       if (!user)
-        return res.status(501).send({ error: "Couldn't find the user" });
-      // remove password from user
-      // mongoose return unecessary data with object so convert it into json
+        return res.status(501).send({ error: "Couldn't Find the User" });
+
+      /** remove password from user */
+      // mongoose return unnecessary data with object so convert it into json
       const { password, ...rest } = Object.assign({}, user.toJSON());
 
       return res.status(201).send(rest);
     });
   } catch (error) {
-    return res.status(404).send({ error: "Cannot find user Data" });
+    return res.status(404).send({ error: "Cannot Find User Data" });
   }
 }
 
